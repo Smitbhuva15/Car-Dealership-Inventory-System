dcb015c7f2b21bf1b1a76b5a568a5a0a6f0be4df
Smitbhuva15 <itssmit123@gmail.com>
2026-07-22 19:15:56 +0530

    feat: implement update vehicle route and controller

--- server/src/controllers/vehicleController.ts
+++ server/src/controllers/vehicleController.ts
@@ -1,6 +1,8 @@
 
 import { Request, Response } from "express";
 import { addVehicle } from "../services/addVehicle";
+import { updateVehicle } from "../services/updateVehicle";
+
 
 export const addVehicleController = async (req: Request, res: Response) => {
   try {
@@ -33,6 +35,34 @@ export const addVehicleController = async (req: Request, res: Response) => {
     return res.status(500).json({
       message: "Internal Server Error.",
       error: "Something went wrong. Please try again later.",
+    });
+  }
+};
+
+export const updateVehicleController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const updated = await updateVehicle(req.params.id as string, req.body);
+
+    return res.status(200).json({
+      message: "Vehicle updated successfully.",
+      vehicle: updated,
+    });
+  } catch (error: any) {
+    if (
+      error.message.includes("Invalid vehicle ID") ||
+      error.message.includes("Vehicle not found") ||
+      error.message.includes("must")
+    ) {
+      return res.status(400).json({
+        error: error.message,
+      });
+    }
+
+    return res.status(500).json({
+      error: "Internal Server Error.",
     });
   }
 };
--- server/src/routes/vehicleRoutes.ts
+++ server/src/routes/vehicleRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import  authenticate from "../middleware/authenticate";
 import  authorize  from "../middleware/authorize"; 
-import { addVehicleController } from "../controllers/vehicleController";
+import { addVehicleController,updateVehicleController } from "../controllers/vehicleController";
 
 const vehicleRoutes = express.Router();
 
 // Admin only routes
 vehicleRoutes.post('/add', authenticate, authorize('admin'), addVehicleController);
+vehicleRoutes.put('/update/:id', authenticate, authorize('admin'),updateVehicleController);
 
 export default vehicleRoutes;
