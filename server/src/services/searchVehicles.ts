fbf95ee0e60d61a876ccab7c338bdf2f1485e42a
Smitbhuva15 <itssmit123@gmail.com>
2026-07-22 13:05:26 +0530

    refactor: improve search vehicles service

--- server/src/services/searchVehicles.ts
+++ server/src/services/searchVehicles.ts
@@ -2,7 +2,35 @@ import { Vehicle } from "../model/Vehicle";
 
 export const searchVehicles = async (filters: any) => {
   try {
-    return await Vehicle.find(filters);
+    const { make, model, category, minPrice, maxPrice } = filters;
+
+    const query: any = {};
+
+    if (make) {
+      query.make = { $regex: make, $options: "i" };
+    }
+
+    if (model) {
+      query.model = { $regex: model, $options: "i" };
+    }
+
+    if (category) {
+      query.category = { $regex: category, $options: "i" };
+    }
+
+    if (minPrice !== undefined || maxPrice !== undefined) {
+      query.price = {};
+
+      if (minPrice !== undefined) {
+        query.price.$gte = Number(minPrice);
+      }
+
+      if (maxPrice !== undefined) {
+        query.price.$lte = Number(maxPrice);
+      }
+    }
+
+    return await Vehicle.find(query);
   } catch (error: any) {
     throw new Error(error.message);
   }
