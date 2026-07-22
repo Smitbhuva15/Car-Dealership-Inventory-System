f016edf16398859b32e44d9996cbf9d1efcc756f
Smitbhuva15 <itssmit123@gmail.com>
2026-07-22 22:16:27 +0530

    chore: update package.json and index.d.ts

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,12 +1,12 @@
-import { IUser } from "../../model/User"; 
+import { IUser } from "../../model/User";
 
 declare global {
-    namespace Express {
-        interface Request {
-            user?: HydratedDocument<IUser>;
-            role?: string;
-        }
+  namespace Express {
+    interface Request {
+      user?: any;
+      role?: string;
     }
+  }
 }
 
-export { };
+export {};
